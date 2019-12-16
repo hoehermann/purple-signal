94f766945c9694936e23214f0485643d3c82d62c
Hermann Höhne <hoehermann@gmx.de>
2019-12-16 22:07:56 +0100

    Internally started Java VM is now remote debuggable.

--- c/libsignal-jni.c
+++ c/libsignal-jni.c
@@ -9,10 +9,11 @@ int purplesignal_init(SignalJVM *sjvm) {
         return 1;
     } else {
         JavaVMInitArgs vm_args;
-        const unsigned int nOptions = 1;
+        const unsigned int nOptions = 2;
         JavaVMOption options[nOptions];
         options[0].optionString = "-Djava.class.path=/opt/signal-cli/lib/argparse4j-0.8.1.jar:/opt/signal-cli/lib/bcprov-jdk15on-1.64.jar:/opt/signal-cli/lib/commons-codec-1.9.jar:/opt/signal-cli/lib/commons-logging-1.2.jar:/opt/signal-cli/lib/core-1.51.0.0.jar:/opt/signal-cli/lib/curve25519-java-0.5.0.jar:/opt/signal-cli/lib/dbus-java-2.7.0.jar:/opt/signal-cli/lib/debug-1.1.1.jar:/opt/signal-cli/lib/hexdump-0.2.1.jar:/opt/signal-cli/lib/httpclient-4.4.jar:/opt/signal-cli/lib/httpcore-4.4.jar:/opt/signal-cli/lib/jackson-annotations-2.9.0.jar:/opt/signal-cli/lib/jackson-core-2.9.9.jar:/opt/signal-cli/lib/jackson-databind-2.9.9.2.jar:/opt/signal-cli/lib/libphonenumber-8.10.7.jar:/opt/signal-cli/lib/okhttp-3.12.1.jar:/opt/signal-cli/lib/okio-1.15.0.jar:/opt/signal-cli/lib/pkix-1.51.0.0.jar:/opt/signal-cli/lib/protobuf-java-2.5.0.jar:/opt/signal-cli/lib/prov-1.51.0.0.jar:/opt/signal-cli/lib/signal-cli-0.6.5.jar:/opt/signal-cli/lib/signal-metadata-java-0.0.3.jar:/opt/signal-cli/lib/signal-protocol-java-2.7.1.jar:/opt/signal-cli/lib/signal-service-java-2.13.9_unofficial_1.jar:/opt/signal-cli/lib/threetenbp-1.3.6.jar:/opt/signal-cli/lib/unix-0.5.1.jar:java/purple_signal.jar"; // TODO: do not hard-code, obviously. maybe https://stackoverflow.com/questions/12489/how-do-you-get-a-directory-listing-in-c with GString and g_string_append_printf
         // TODO: find own location, pass it on to -Djava.library.path=. maybe with https://stackoverflow.com/questions/43409167/find-location-of-loaded-shared-library-from-in-that-shared-library
+        options[1].optionString = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10044"; // TODO: conditionally enable this only in debug builds. Or if Pidgin was started in debug mode?
         vm_args.options = options;
         vm_args.nOptions = nOptions;
         vm_args.version  = JNI_VERSION_1_8;
@@ -46,9 +47,9 @@ int purplesignal_login(SignalJVM sjvm, PurpleSignal *ps, uintptr_t connection, c
         printf("signal: Failed to create an instance of PurpleSignal.\n");
         return 0;
     }
-    jmethodID method = (*sjvm.env)->GetMethodID(sjvm.env, cls, "receiveMessages", "()V");
+    jmethodID method = (*sjvm.env)->GetMethodID(sjvm.env, cls, "startReceiving", "()V");
     if (constructor == NULL) {
-        printf("signal: Failed to find method receiveMessages.\n");
+        printf("signal: Failed to find method startReceiving.\n");
         return 0;
     }
     (*sjvm.env)->CallVoidMethod(sjvm.env, ps->instance, method);
