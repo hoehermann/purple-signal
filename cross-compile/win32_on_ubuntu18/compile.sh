4302e5de71d7933a6b92f467d22029292d1c80d8
Hermann Höhne <hoehermann@gmx.de>
2020-04-07 19:37:31 +0200

    Should now be reasonably clean.

---
+++ cross-compile/win32_on_ubuntu18/compile.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+export PKG_CONFIG_LIBDIR=$(realpath ../../gtk+/lib/pkgconfig)
+export PKG_CONFIG_PATH=$(realpath ../../pidgin-2.13.0/libpurple/lib/pkgconfig)
+
+cmake \
+-DCMAKE_TOOLCHAIN_FILE=../cross-compile/win32_on_ubuntu18/toolchain.cmake \
+-DCMAKE_MODULE_PATH=../cross-compile/win32_on_ubuntu18 \
+-DPKG_CONFIG_EXECUTABLE="/usr/bin/pkg-config\;--define-prefix" \
+-DJAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 \
+-DJAVA_HOME_WIN32=../../jdk8u232-b09 \
+-DSIGNAL_CLI_LIB_DIR=../../signal-cli-0.6.5/lib \
+-DCMAKE_BUILD_TYPE=Debug \
+..
