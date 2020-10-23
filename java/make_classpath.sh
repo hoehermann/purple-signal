5e7822f55b30f1a3326a190eb22a881c9b10b450
Hermann Höhne <hoehermann@gmx.de>
2020-10-23 17:17:28 +0200

    Have a .classpath generator for developing with eclipse.

---
+++ java/make_classpath.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# makes a .classpath file for use in eclipse (this directory is the project root)
+
+if [[ -z "$@" ]]
+then
+  echo "Usage: $0 /wherever/signal-cli/lib/* > .classpath"
+  exit 1
+fi
+
+cat <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<classpath>
+	<classpathentry kind="src" path="src"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER">
+		<attributes>
+			<attribute name="module" value="true"/>
+		</attributes>
+	</classpathentry>
+	<classpathentry kind="output" path="bin"/>
+EOF
+for i in $@
+do
+  echo '	<classpathentry kind="lib" path="'$i'"/>'
+done
+echo '</classpath>'
