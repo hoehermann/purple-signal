ecbf95455a597874e235071d3679998caf656c3c
Hermann Höhne <hoehermann@gmx.de>
2021-01-18 23:19:33 +0100

    Add option to ignore system messages.

--- c/libsignal.hpp
+++ c/libsignal.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
-#define SIGNAL_PLUGIN_ID "prpl-hehoe-signal"
+#ifndef SIGNAL_PLUGIN_VERSION
+#error Must set SIGNAL_PLUGIN_VERSION in Makefile
+#endif
+
+extern const char * const SIGNAL_PLUGIN_ID;
+extern const char * const SIGNAL_PLUGIN_WEBSITE;
+extern const char * const SIGNAL_OPTION_LIBDIR;
+extern const char * const SIGNAL_DEFAULT_LIBDIR;
+extern const char * const SIGNAL_OPTION_SHOW_SYSTEM;
+extern const bool SIGNAL_DEFAULT_SHOW_SYSTEM;
